71f4d6fb26440edf590abab55941beaaf4fd50fa
shelly8525 <shelly8525@yahoo.com.tw>
2015-05-12 10:02:25 +0800

    做出第一個panel

--- src/CircleCalculator.java
+++ src/CircleCalculator.java
@@ -4,6 +4,23 @@ import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
 public class CircleCalculator extends JFrame {
+	public CircleCalculator(){
+		JPanel p1 = new JPanel();
+		p1.add(new JLabel("Circle:"));
+		
+		JPanel p2 = new JPanel(new FlowLayout());
+		p2.add(new JLabel("Radius:"));
+		p2.add(new JTextField());
+		p2.add(new JButton("­pºâ"));
+		p2.add(new JButton("²M°£"));
+		
+		JPanel p3 = new JPanel(new GridLayout(2,2,2,2));
+		p3.add(new JLabel("The area is:"));
+		p3.add(new JTextField());
+		p3.add(new JLabel("The perimeter is:"));
+		p3.add(new JTextField());
+		
+	}
 	public static void main(String[] args){
 		CircleCalculator frame = new CircleCalculator();
 		frame.pack();
