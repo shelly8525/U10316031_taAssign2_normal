189259460700fe8901a11779d1102a49371b320a
shelly8525 <shelly8525@yahoo.com.tw>
2015-05-12 09:48:03 +0800

    把frame先做好

---
+++ src/CircleCalculator.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+public class CircleCalculator extends JFrame {
+	public static void main(String[] args){
+		CircleCalculator frame = new CircleCalculator();
+		frame.pack();
+		frame.setTitle("U10316031_GeoCalculate");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
